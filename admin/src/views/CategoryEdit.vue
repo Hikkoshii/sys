ddb99e8e85c9cb671d10a3fb2c084a67bfbde44f
Hikkoshii <iloveumika@qq.com>
2021-04-07 17:45:00 +0800

    33

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,9 @@
   <div class="about">
     <h1>{{id ? "编辑" : "新建"}}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+        <!-- <el-form-item label="上级分类">
+            <el-input v-model="model.parent"></el-input>
+        </el-form-item>  -->
         <el-form-item label="名称">
             <el-input v-model="model.name"></el-input>
         </el-form-item> 
