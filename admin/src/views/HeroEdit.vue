2e6683de68c4e188c9af84a6ae67ebd251738aec
Hikkoshii <iloveumika@qq.com>
2021-04-10 20:49:18 +0800

    英雄编辑

--- admin/src/views/HeroEdit.vue
+++ admin/src/views/HeroEdit.vue
@@ -5,6 +5,9 @@
         <el-form-item label="名称">
             <el-input v-model="model.name"></el-input>
         </el-form-item> 
+        <el-form-item label="称号">
+            <el-input v-model="model.title"></el-input>
+        </el-form-item> 
         <el-form-item label="头像">
             <el-upload
             class="avatar-uploader"
@@ -14,6 +17,51 @@
             <img v-if="model.avatar" :src="model.avatar" class="avatar">
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
+        </el-form-item>
+        <el-form-item label="类型">
+            <el-select v-model="model.categories" multiple>
+                <el-option v-for="item of categories"
+                :label="item.name"
+                :value="item._id"
+                :key="item._id"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="难度">
+            <el-rate style="magin-top:0.6rem" :max="9" show-score v-model="model.scores.difficult"></el-rate>
+        </el-form-item>  
+        <el-form-item label="技能">
+            <el-rate style="magin-top:0.6rem" :max="9" show-score v-model="model.scores.skills"></el-rate>
+        </el-form-item>  
+        <el-form-item label="攻击">
+            <el-rate style="magin-top:0.6rem" :max="9" show-score v-model="model.scores.attack"></el-rate>
+        </el-form-item>  
+        <el-form-item label="生存">
+            <el-rate style="magin-top:0.6rem" :max="9" show-score v-model="model.scores.survive"></el-rate>
+        </el-form-item>  
+        <el-form-item label="顺风出装">
+            <el-select v-model="model.items1" multiple>
+                <el-option v-for="item of items"
+                :label="item.name"
+                :value="item._id"
+                :key="item._id"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="逆风出装">
+            <el-select v-model="model.items2" multiple>
+                <el-option v-for="item of items"
+                :label="item.name"
+                :value="item._id"
+                :key="item._id"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="使用技巧">
+            <el-input type="textarea" v-model="model.usageTips"></el-input>
+        </el-form-item>
+        <el-form-item label="对抗技巧">
+            <el-input type="textarea" v-model="model.battleTips"></el-input>
+        </el-form-item>
+        <el-form-item label="团战思路">
+            <el-input type="textarea" v-model="model.teamTips"></el-input>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" native-type="submit">保存</el-button>
@@ -29,9 +77,17 @@ export default {
     },
     data() {
         return {
+            categories: [],
+            items: [],
             model: {
                 name:'',
-                avatar:''
+                avatar:'',
+                scores:{
+                    difficult: 0,
+                    skills: 0,
+                    attack: 0,
+                    survive: 0
+                },
             }
         };
     },
@@ -55,11 +111,21 @@ export default {
         },
         async fetch(){
             const res = await this.$http.get(`rest/heroes/${this.id}`)// eslint-disable-line no-unused-vars
-            this.model = res.data
+            this.model = Object.assign({}, this.model, res.data);
+        },
+        async fetchCategories(){
+            const res = await this.$http.get(`rest/categories`)// eslint-disable-line no-unused-vars
+            this.categories = res.data;
+        },
+        async fetchItems(){
+            const res = await this.$http.get(`rest/items`)// eslint-disable-line no-unused-vars
+            this.items = res.data;
         },
     },
     created(){
-        this.id && this.fetch()
+        this.fetchCategories()
+        this.fetchItems()
+        this.id && this.fetch();
     }
 }
 </script>
