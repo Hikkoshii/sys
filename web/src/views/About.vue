ee87b52640865ad50f723584b10d2bd03c11ffcd
Hikkoshii <iloveumika@qq.com>
2021-04-12 15:43:20 +0800

    主页框架和顶部菜单

---
+++ web/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="top">
+
+    </div>
+    <div class="nav">
+
+    </div>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      主页
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
