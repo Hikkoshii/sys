5f5183dd7c68818a00247ee925270178a728158a
Hikkoshii <iloveumika@qq.com>
2021-04-12 20:37:30 +0800

    使用精灵图片

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -11,7 +11,20 @@
       <img class="w-100" src="../assets/images/210794580bb9303653804bb7b482f2a4.jpeg">
     </swiper-slide>
     <div class="swiper-pagination pagination-home text-right px-3 pb-2" slot="pagination"></div>
-  </swiper>
+    </swiper>
+  <!-- 幻灯片结束 -->
+    <div class="nav-icons bg-white mt-3 pt-3 text-center text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n" >
+        <i class="sprite sprite-news"></i>
+        <div class="py-2">爆料站</div>
+      </div>
+      </div>   
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-2  "></i>
+        <span>收起</span>
+        </div>  
+    </div>
   </div>
 </template>
 
@@ -42,6 +55,17 @@ export default {
     &.swiper-pagination-bullet-active {
       background: map-get($colors, "info");
     }
-  }
+  }  
 }
+.nav-icons{
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+    .nav-item{
+      width: 25%;
+      border-right: 1px solid $border-color;
+      &:nth-child(4n){
+        border-right: none;
+      }
+    }
+  }
 </style>
