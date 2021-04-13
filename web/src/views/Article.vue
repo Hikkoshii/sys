84b56bfdcdd3bbeff5b0a861dfc582b2b72dd013
Hikkoshii <iloveumika@qq.com>
2021-04-13 21:52:50 +0800

    新闻详情页-完善

--- web/src/views/Article.vue
+++ web/src/views/Article.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="page-article" v-if="model">
-    <div class="d-flex py-3 px-2">
-      <div class="iconfont icont-Back"></div>
-      <strong class="flex-1 text-ellipsis">
+    <div class="d-flex py-3 px-2 border-bottom">
+      <div class="iconfont icon-Back text-blue"></div>
+      <strong class="flex-1 text-blue pl-2 text-ellipsis">
         {{model.title}}
       </strong>
       <div class="text-grey fs-xs">
         2021-04-12
       </div>
     </div>
-    <div v-html="model.body"></div>
+    <div v-html="model.body" class="px-3 body fs-lg"></div>
+    <div class="px-3 border-top py-3">
+      <div class="d-flex ai-center"> 
+        <i class="iconfont icon-menu1"></i>
+        <strong class="text-blue fs-lg ml-1">相关资讯</strong>
+      </div>
+      <div class="pt-2 fs-lg ">
+      <router-link class="py-1"
+      tag="div"
+      :to="`/articles/${item._id}`"
+      v-for="item in model.related"
+      :key="item._id">
+      {{item.title}}
+      </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +37,12 @@ export default {
   data(){
     return{
       model: null
+    };
+  },
+  watch:{
+    // id: 'fetch',
+    id(){
+      this.fetch()
     }
   },
   methods: {
@@ -35,3 +56,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.page-article{
+  .icon-Back{
+    font-size: 1.6923rem;
+  }
+  .body{
+    img{
+      max-width: 100%;
+      height: auto;
+    }
+    iframe{
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
