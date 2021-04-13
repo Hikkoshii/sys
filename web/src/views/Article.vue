1e9a64f5c366617f13a0115d080a69f88ea26e13
Hikkoshii <iloveumika@qq.com>
2021-04-13 20:58:26 +0800

    新闻详情页

---
+++ web/src/views/Article.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page-article" v-if="model">
+    <div class="d-flex py-3 px-2">
+      <div class="iconfont icont-Back"></div>
+      <strong class="flex-1 text-ellipsis">
+        {{model.title}}
+      </strong>
+      <div class="text-grey fs-xs">
+        2021-04-12
+      </div>
+    </div>
+    <div v-html="model.body"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props:{
+    id: { required: true }
+  },
+  data(){
+    return{
+      model: null
+    }
+  },
+  methods: {
+    async fetch(){
+      const res = await this.$http.get(`articles/${this.id}`)
+      this.model = res.data
+    }
+  },
+  created(){
+    this.fetch()
+  }
+}
+</script>
